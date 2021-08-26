cac3236d3a7f53cee5e7f2e10e519501ae461b38
Kamal Rathi <kamalrathi049@gmail.com>
2021-08-26 22:15:43 +0530

    Initial commit

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class API_objects(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class API_objects_details(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
